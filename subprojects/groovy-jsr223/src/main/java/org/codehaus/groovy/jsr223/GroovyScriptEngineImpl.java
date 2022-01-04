925222733b10c7c2f2ca53e5e4a23d05b76f22d8
tmoore <tmoore@spatial.ca>
2022-01-04 15:51:15 -0500

    Change the JSR223 script engine so that it will remember import statements between invocations.  This is based on code from the scijava team (https://github.com/scijava/scripting-groovy)

--- subprojects/groovy-jsr223/src/main/java/org/codehaus/groovy/jsr223/GroovyScriptEngineImpl.java
+++ subprojects/groovy-jsr223/src/main/java/org/codehaus/groovy/jsr223/GroovyScriptEngineImpl.java
@@ -43,8 +43,37 @@
  *  KIND, either express or implied.  See the License for the
  *  specific language governing permissions and limitations
  *  under the License.
+ *
+ * Overrides used to capture the AST and to retain imports taken from 
+ * https://github.com/scijava/scripting-groovy under the following 
+ * copyright and license:
+ * 
+ * Copyright (C) 2014 - 2021 SciJava developers.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 package org.codehaus.groovy.jsr223;
+
+import java.security.CodeSource;
 
 import groovy.lang.Binding;
 import groovy.lang.Closure;
@@ -55,7 +84,10 @@ import groovy.lang.MissingMethodException;
 import groovy.lang.MissingPropertyException;
 import groovy.lang.Script;
 import groovy.lang.Tuple;
+import org.codehaus.groovy.ast.CompileUnit;
+import org.codehaus.groovy.ast.ModuleNode;
 import org.codehaus.groovy.control.CompilationFailedException;
+import org.codehaus.groovy.control.CompilationUnit;
 import org.codehaus.groovy.control.CompilerConfiguration;
 import org.codehaus.groovy.runtime.InvokerHelper;
 import org.codehaus.groovy.runtime.MetaClassHelper;
@@ -104,20 +136,45 @@ public class GroovyScriptEngineImpl extends AbstractScriptEngine implements Comp
     // counter used to generate unique global Script class names
     private static int counter;
 
+    // Temporarily save the AST between compilation and evaluation
+    private static final ThreadLocal<CompileUnit> asts = new ThreadLocal<>();
+
+    // A class to keep track of cummulative imports
+    private ImportRetainer importRetainer;
+
     static {
         counter = 0;
     }
 
     public GroovyScriptEngineImpl() {
-        this(VMPluginFactory.getPlugin().doPrivileged(new PrivilegedAction<GroovyClassLoader>() {
+        importRetainer = new ImportRetainer();
+        loader =
+            VMPluginFactory.getPlugin().doPrivileged(new PrivilegedAction<GroovyClassLoader>() {
             @Override
             public GroovyClassLoader run() {
-                return new GroovyClassLoader(getParentLoader(), new CompilerConfiguration(CompilerConfiguration.DEFAULT));
+                CompilerConfiguration compilerConfig = new CompilerConfiguration(CompilerConfiguration.DEFAULT);
+		compilerConfig.addCompilationCustomizers(importRetainer);
+                return new GroovyClassLoader(getParentLoader(), compilerConfig) {
+                    @Override
+                    protected CompilationUnit createCompilationUnit(CompilerConfiguration config, CodeSource source) {
+                        // Create the compilation unit as usual.
+                        final CompilationUnit unit = super.createCompilationUnit(config, source);
+
+                        // Obtain the AST that now exists as part of the unit creation.
+                        final CompileUnit ast = unit.getAST();
+                        
+                        // Save the AST into a ThreadLocal, for post-eval retrieval.
+                        asts.set(ast);
+                        
+                        return unit;
+                    }
+                };
             }
-        }));
+        });
     }
 
-    public GroovyScriptEngineImpl(GroovyClassLoader classLoader) {
+    private GroovyScriptEngineImpl(GroovyClassLoader classLoader) {
+        importRetainer = new ImportRetainer(); 
         if (classLoader == null) throw new IllegalArgumentException("GroovyClassLoader is null");
         this.loader = classLoader;
     }
@@ -324,7 +381,9 @@ public class GroovyScriptEngineImpl extends AbstractScriptEngine implements Comp
                     }
                 });
 
-                return scriptObject.run();
+                Object result = scriptObject.run();
+                saveImports();
+                return result;
             }
         } catch (Exception e) {
             throw new ScriptException(e);
@@ -469,4 +528,20 @@ public class GroovyScriptEngineImpl extends AbstractScriptEngine implements Comp
         }
         return buf.toString();
     }
-}
+
+    private void saveImports() {
+        // Retrieve the AST that was built during compilation+evaluation.
+        final CompileUnit ast = asts.get();
+        if (ast == null)
+            return;
+        asts.remove();
+        
+        // Extract the imports present in the AST, and save them for next time.
+        for (final ModuleNode m : ast.getModules()) {
+            m.getImports().forEach(ipt -> importRetainer.retainImport(ipt));
+            m.getStarImports().forEach(ipt -> importRetainer.retainStarImport(ipt));
+            m.getStaticImports().values().forEach(ipt -> importRetainer.retainStaticImport(ipt));
+            m.getStaticStarImports().values().forEach(ipt -> importRetainer.retainStaticStarImport(ipt));
+        }
+    }
+} 
